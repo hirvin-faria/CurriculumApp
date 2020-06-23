7249a1834e82b063013496b628046ca71a590c4d
hirvin-faria <hirvin.nogueira.faria@gmail.com>
2020-06-23 19:20:24 -0300

    :construction: Adicionando Bundle Intent

--- app/src/main/java/com/curriculumapp/AddCurriculum.kt
+++ app/src/main/java/com/curriculumapp/AddCurriculum.kt
@@ -1,9 +1,9 @@
 package com.curriculumapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class AddCurriculum : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +11,12 @@ class AddCurriculum : AppCompatActivity() {
         setContentView(R.layout.activity_add_curriculum)
 
         val ButtonAddCurriculum = findViewById<Button>(R.id.buttonAdd)
+
         ButtonAddCurriculum.setOnClickListener {
+            val bundle = Bundle()
+            bundle.putString("Nome", "Hirvin Faria")
             val intentGoToReview = Intent(this, ReviewActivity::class.java)
+            intentGoToReview.putExtras(bundle)
             startActivity(intentGoToReview)
         }
     }
--- app/src/main/java/com/curriculumapp/ReviewActivity.kt
+++ app/src/main/java/com/curriculumapp/ReviewActivity.kt
@@ -1,13 +1,23 @@
 package com.curriculumapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class ReviewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_review)
 
+        val intent = intent.getExtras()
+        //val bundle: Bundle = intent.getBundleExtra("Nome")
+
+
+        var title = findViewById<TextView>(R.id.reviewTitle);
+        if (intent != null) {
+            title.setText(intent.getString("Nome"))
+        };
 
     }
 }
