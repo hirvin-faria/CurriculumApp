0a288854c64e738e1c3169de90a7da7919edfb64
hirvin-faria <hirvin.nogueira.faria@gmail.com>
2020-06-23 21:42:07 -0300

    :sparkles: Adicionando dados da tela de review

--- app/src/main/java/com/curriculumapp/ReviewActivity.kt
+++ app/src/main/java/com/curriculumapp/ReviewActivity.kt
@@ -2,6 +2,7 @@ package com.curriculumapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,14 +11,34 @@ class ReviewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_review)
 
-        val intent = intent.getExtras()
-        //val bundle: Bundle = intent.getBundleExtra("Nome")
+        //Obtendo campos de textview da revisao
+        val revName = findViewById<TextView>(R.id.revTextViewName)
+        val revIdade = findViewById<TextView>(R.id.revTextViewIdade)
+        val revObjetivo = findViewById<TextView>(R.id.revTextViewObjetivo)
+        val revFormacao = findViewById<TextView>(R.id.revTextViewFormacao)
+        val revExperiencia = findViewById<TextView>(R.id.revTextViewExperiencia)
+
+        //Resposta do intent
+        val respoBundle = intent.getExtras()
 
 
-        var title = findViewById<TextView>(R.id.reviewTitle);
-        if (intent != null) {
-            title.setText(intent.getString("Nome"))
+        if (respoBundle != null) {
+            revName.setText("Nome: ${respoBundle.getString("name")}")
+            revIdade.setText("Idade: ${respoBundle.getString("idade")}")
+            revObjetivo.setText("Objetivo: ${respoBundle.getString("objetivo")}")
+            revFormacao.setText("Formação: ${respoBundle.getString("formacao")}")
+            revExperiencia.setText("Experiência: ${respoBundle.getString("experiencia")}")
         };
 
+
+        val revBtnAddConfirm = findViewById<Button>(R.id.revBtnAddConfirm)
+
+        //Adiciona um listener do tipo onClick para o Botão
+        revBtnAddConfirm.setOnClickListener {
+
+            //Criacao da intent
+            val intentFinishAdd = Intent(this, MainActivity::class.java)
+            startActivity(intentFinishAdd)
+        }
     }
 }
